6a0477a85ebafb9e3702e269308420fd60a2aa26
yarikpo <yaroslavpopovich04@gmail.com>
2022-07-10 19:42:17 +0300

    routers deleted repeated options

--- routes/rout.router.js
+++ routes/rout.router.js
@@ -7,60 +7,41 @@ router.get('/test', (req, res) => {
     res.send("Hello");
 });
 
-
+function getOptions(dir) {
+  return {
+      root: path.join(__dirname, dir),
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+  };
+}
 
 
 router.get('/', (req, res) => {
-    let options = {
-        root: path.join(__dirname, '..'),
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
-        }
-    };
+    let options = getOptions('..');
 
     let fileName = './src/public/index.html';
     res.sendFile(fileName, options, () => console.log('Sent:', fileName));
 });
 
 router.get('/index.js', (req, res) => {
-    let options = {
-        root: path.join(__dirname, '..'),
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
-        }
-    };
+    let options = getOptions('..');
 
     let fileName = './src/public/index.js';
     res.sendFile(fileName, options, () => console.log('Sent:', fileName));
 });
 
 router.get('/index.css', (req, res) => {
-    let options = {
-        root: path.join(__dirname, '..'),
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
-        }
-    };
+    let options = getOptions('..');
 
     let fileName = './src/public/index.css';
     res.sendFile(fileName, options, () => console.log('Sent:', fileName));
 });
 
 router.get('/:name', function (req, res, next) {
-    var options = {
-      root: path.join(__dirname, '../src/public/pages'),
-      dotfiles: 'deny',
-      headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-      }
-    };
+    var options = getOptions('../src/public/pages');
   
     var fileName = req.params.name;
     res.sendFile(fileName, options, function (err) {
